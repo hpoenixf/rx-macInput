55f8593c4b98f7fe0dfc54a84fbd96d2e65005a3
hpoenixf <hpoenixf@gmail.com>
2017-06-23 23:45:31 +0800

    'setStateAsync'

--- src/App.js
+++ src/App.js
@@ -13,14 +13,13 @@ class App extends Component {
   componentDidMount () {
     this.keydownValue = new Rx.Subject()
     let keydownValue = this.keydownValue.map(e => e.key.toUpperCase())
-    this.sa = keydownValue.filter(value => value.length === 1 && value.match(/[0-9A-F]/)).subscribe(value => {
-      this.setColon('before', () => {
-        this.insertValue(value, () => {
-          this.setColon()
-        })
-      });
+    this.sa = keydownValue.filter(value => value.length === 1 && value.match(/[0-9A-F]/)).subscribe(async value => {
+      await this.setColon('before')
+      await this.insertValue(value)
+      await this.setColon()
+      this.goPos()
     })
-    this.sb = keydownValue.filter(value => value === 'BACKSPACE').subscribe(() => {this.deleteValue()})
+    this.sb = keydownValue.filter(value => value === 'BACKSPACE').subscribe(async () => {await this.deleteValue();this.goPos()})
     this.sc = keydownValue.filter(value => value === 'ARROWLEFT').subscribe(() => this.moveLeft())
     this.sd = keydownValue.filter(value => value === 'ARROWRIGHT').subscribe(() => this.moveRight())
     this.se = keydownValue.subscribe(() => setTimeout(this.goPos, 0))
@@ -39,41 +38,40 @@ class App extends Component {
     e.preventDefault()
     this.keydownValue.onNext(e)
   }
-  insertValue = (value,cb) => {
+  async insertValue (value) {
     if (this.state.value.length !== 17) {
-      this.setState({...this.state,value: this.state.value.slice(0, this.state.pos) + value + this.state.value.slice(this.state.pos, this.state.value.length)}, () => {
-        if (this.state.value.split(':').length === 7) {
-          this.setState({...this.state, value: this.state.value.slice(0, this.state.value.lastIndexOf(':'))}, () => this.setPos(this.state.pos + 1,cb))
-        } else {
-          this.setPos(this.state.pos + 1,cb)
-        }
-      })
-  }}
-  deleteValue = () => {
+      await this.setStateAsync({...this.state,value: this.state.value.slice(0, this.state.pos) + value + this.state.value.slice(this.state.pos, this.state.value.length)})
+      if (this.state.value.split(':').length === 7) {
+        await this.setState({...this.state, value: this.state.value.slice(0, this.state.value.lastIndexOf(':'))})
+      }
+      await this.setPos(this.state.pos + 1)
+    }
+  }
+  async deleteValue () {
     if (this.state.value.length && this.state.pos) {
-      this.setState({...this.state, value: this.state.value.slice(0, this.state.pos - 1) + this.state.value.slice(this.state.pos, this.state.value.length)}, () =>{
-        this.setPos(this.state.pos - 1, () => {
-          if (this.isLastColon()) {
-            this.deleteValue()
-          }
-        })
-      })
+      await this.setStateAsync({...this.state, value: this.state.value.slice(0, this.state.pos - 1) + this.state.value.slice(this.state.pos, this.state.value.length)})
+      await this.setPos(this.state.pos - 1)
+      if (this.isLastColon()) {
+        this.deleteValue()
+      }
     }
   }
-  setColon = (type, cb) => {
+  async setColon (type) {
     if (this.state.value.length &&  !(this.state.value.slice(0, this.state.pos).replace(/:/g, '').length%2) && (type === 'before' ?  !this.isLastColon() : !this.isNearColon())) {
-      this.insertValue(':', cb)
-    } else {
-      cb && cb()
+      await this.insertValue(':')
     }
   }
-  setPos = (pos = this.state.value.length, cb) => this.setState({...this.state, pos},() => cb && cb())
+  async setPos (pos = this.state.value.length, cb) { await this.setStateAsync({...this.state, pos})}
   moveLeft = () => this.state.pos > 0 && this.setState({...this.state, pos: this.state.pos - 1})
   moveRight = () => this.state.pos !== this.state.value.length && this.setState({...this.state, pos: this.state.pos + 1})
   goPos = () => ReactDOM.findDOMNode(this.refs.t).setSelectionRange(this.state.pos, this.state.pos)
   isNearColon = () => this.isLastColon() || this.state.value.charAt(this.state.pos ) === ':'
   isLastColon = () => this.state.value.charAt(this.state.pos - 1) === ':'
-
+  setStateAsync = state => {
+    return new Promise(resolve => {
+      this.setState(state,resolve)
+    })
+  }
   render() {
     return (
       <div className="App">
